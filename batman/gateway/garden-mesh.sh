334775957a2b0b89c96a085008659633d79d90a9
Shri Hari <shrihari@seatris.com>
2019-11-26 01:34:55 +0100

    documentation for batman

--- batman/gateway/garden-mesh.sh
+++ batman/gateway/garden-mesh.sh
@@ -1,11 +1,12 @@
 sudo batctl if add wlan0
-
+# Setting particular node as gateway
 sudo batctl gw_mode server
-
+# Port forwarding between wlan0 and Ethernet
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -o enxb827ebb206b4 -j MASQUERADE
-sudo iptables -A FORWARD -i enxb827ebb206b4 -o bat0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i bat0 -o enxb827ebb206b4 -j ACCEPT
-
+ETH=`ls /sys/class/net | awk '/en/{print $0}'`
+sudo iptables -t nat -A POSTROUTING -o $ETH -j MASQUERADE
+sudo iptables -A FORWARD -i $ETH -o bat0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i bat0 -o $ETH -j ACCEPT
+# Activate the interface for B.A.T.M.A.N
 sudo ifconfig wlan0 up
 sudo ifconfig bat0 up
