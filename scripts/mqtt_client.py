84f89a2a837322227c8c40cab57b5e2caf10cafb
peewee <peewee.manzer@outlook.de>
2019-12-14 13:56:16 +0100

    update publish sensor data

--- scripts/mqtt_client.py
+++ scripts/mqtt_client.py
@@ -1,14 +1,13 @@
 import paho.mqtt.client as mqtt
-import datetime
 import json
-import random
 import time
+import getpass
 
 
 class ServerCom:
     SENSORS = 'sensors'
 
-    def __init__(self, server_port=1883, server_host='ec2-18-185-135-181.eu-central-1.compute.amazonaws.com'):
+    def __init__(self, server_port=1883, server_host='localhost'):
         self.server_port = server_port
         self.server_host = server_host
         self.mqtt_c = mqtt.Client("gateway")
@@ -16,19 +15,17 @@ class ServerCom:
 
     def connect(self):
         def on_connect(mqttc, obj, flags, rc):
-            self.mqtt_c.subscribe(self.SENSORS, qos=2)
+            # self.mqtt_c.subscribe(self.SENSORS, qos=2)
             print('Connected')
 
         def on_publish(mqttc, obj, mid):
-            print("published", mid)
+            print("published", mqttc, obj, mid)
             pass
 
         def on_message(client, userdata, msg):
             print('msg: ', msg)
             msg_data = json.loads(msg.payload.decode('utf-8'))
             print('msg_data: ', msg_data)
-            msg_data['timestamp'] = datetime.datetime.fromtimestamp(int(msg_data['timestamp']) / 1e3)
-            print(msg_data)
 
         self.mqtt_c.on_connect = on_connect
         self.mqtt_c.on_publish = on_publish
@@ -36,18 +33,20 @@ class ServerCom:
         self.mqtt_c.on_message = on_message
         self.mqtt_c.loop_start()
 
-    def publish_temperature(self, temperature):
-        ret = self.mqtt_c.publish(self.SENSORS, str(temperature), qos=2)
+    def publish_sensor_data(self, sensor, value):
+        device_id = getpass.getuser()
+        msg = '{"device_id": "' + device_id + '", "measurement": "' + sensor + '", "data": ' + str(
+            value) + ', "timestamp": ' + str(int(time.time())) + '}'
+        print(msg)
+        ret = self.mqtt_c.publish(self.SENSORS, msg, qos=2)
         ret.wait_for_publish()
 
+# if __name__ == "__main__":
+# com = ServerCom(server_host='ec2-3-122-180-139.eu-central-1.compute.amazonaws.com')
 
-if __name__ == "__main__":
-    com = ServerCom()
-
-    msg = '{"sensor": "moisture", "data": "' + str(random.randint(1, 101)) + '"}'
-    com.publish_temperature(msg)
-    msg = '{"sensor": "moisture", "data": "' + str(random.randint(1, 101)) + '"}'
-    com.publish_temperature(msg)
-    msg = '{"sensor": "moisture", "data": "' + str(random.randint(1, 101)) + '"}'
-    com.publish_temperature(msg)
-    x = input()
+# com.publish_sensor_data('water_level', str(random.randint(1, 101)))
+# time.sleep(1)
+# com.publish_sensor_data('water_level', str(random.randint(1, 101)))
+# time.sleep(1)
+# com.publish_sensor_data('water_level', str(random.randint(1, 101)))
+# time.sleep(1)
