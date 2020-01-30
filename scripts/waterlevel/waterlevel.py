7d50b7e2b1722598b23a65e6a82735467441dd33
peewee <peewee.manzer@outlook.de>
2020-01-30 11:38:20 +0100

    clean up

---
+++ scripts/waterlevel/waterlevel.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+from mqtt_client import ServerCom
+
+# change these as desired - they're the pins connected from the
+# SPI port on the ADC to the Cobbler
+SPICLK = 11
+SPIMISO = 9
+SPIMOSI = 10
+SPICS = 8
+
+# photoresistor connected to adc #0
+photo_ch = 0
+
+
+class WaterLevel:
+    def __init__(self):
+
+        GPIO.setwarnings(False)
+        GPIO.cleanup()  # clean up at the end of your script
+        GPIO.setmode(GPIO.BCM)  # to specify whilch pin numbering system
+        # set up the SPI interface pins
+        GPIO.setup(SPIMOSI, GPIO.OUT)
+        GPIO.setup(SPIMISO, GPIO.IN)
+        GPIO.setup(SPICLK, GPIO.OUT)
+        GPIO.setup(SPICS, GPIO.OUT)
+        self.mqtt_client = ServerCom()
+        self.collector()
+
+    # read SPI data from MCP3008(or MCP3204) chip,8 possible adc's (0 thru 7)
+    def readadc(self, adcnum, clockpin, mosipin, misopin, cspin):
+        if (adcnum > 7) or (adcnum < 0):
+            return -1
+        GPIO.output(cspin, True)
+
+        GPIO.output(clockpin, False)  # start clock low
+        GPIO.output(cspin, False)  # bring CS low
+
+        commandout = adcnum
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3  # we only need to send 5 bits here
+        for i in range(5):
+            if commandout & 0x80:
+                GPIO.output(mosipin, True)
+            else:
+                GPIO.output(mosipin, False)
+            commandout <<= 1
+            GPIO.output(clockpin, True)
+            GPIO.output(clockpin, False)
+
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+            GPIO.output(clockpin, True)
+            GPIO.output(clockpin, False)
+            adcout <<= 1
+            if (GPIO.input(misopin)):
+                adcout |= 0x1
+
+        GPIO.output(cspin, True)
+
+        adcout >>= 1  # first bit is 'null' so drop it
+        return adcout
+
+    def collector(self):
+        print('start collecting data')
+        while True:
+            adc_value = self.readadc(photo_ch, SPICLK, SPIMOSI, SPIMISO, SPICS)
+            self.mqtt_client.publish_sensor_data("water_level", adc_value)
+            time.sleep(2)
+
+
+if __name__ == '__main__':
+    waterlevel = WaterLevel()
