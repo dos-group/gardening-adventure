59d12a59e6d608ba24372cff161d574514971889
peewee <peewee.manzer@outlook.de>
2020-02-13 17:32:09 +0100

    update light

--- scripts/light/rgb_light.py
+++ scripts/light/rgb_light.py
@@ -1,32 +1,30 @@
 import spidev
 import ws2812
+import getpass
+import board
+import neopixel
 
 from mqtt_subscriber import Subscriber
 
 
-class RGB_Light:
-    LIGHT_TOPIC = "actor/rgb_light/"
+class LightRGB:
+    LIGHT_TOPIC = "sensor/light/"
 
-    def __init__(self, light_id, num_led=12):
+    def __init__(self, num_led=12):
         self.num_led = num_led
-        self.spi = spidev.SpiDev()
-        self.spi.open(0, 0)
-        self.mqtt_client = Subscriber(self.LIGHT_TOPIC + str(light_id), self.callback)
+        self.pixels = neopixel.NeoPixel(board.D18, self.num_led)
+        self.mqtt_client = Subscriber(self.LIGHT_TOPIC + getpass.getuser(), self.callback)
 
     def callback(self, msg):
         print(msg['data'])
-        if len(msg['data']) == self.num_led:
-            ws2812.write2812(self.spi, msg['data'])
+        if msg['data'] > 1000:
+            brightness = 255
+        else:
+            brightness = int(msg['data'] * 0.255)
 
+        self.pixels[0:] = [[brightness, brightness, brightness] for i in range(self.num_led)]
 
-# cyan
-# ws2812.write2812(spi, [[10,0,100]])
-# off
-# ws2812.write2812(spi, [[0,0,0]])
-# green
-# ws2812.write2812(spi, [[10,0,0]])
-# very bright green
 
 if __name__ == "__main__":
-    li = RGB_Light(1)
+    li = LightRGB()
 #    com = Subscriber('actor/rgb_light/1', RGB_Light.callback, server_host='broker.hivemq.com', server_port=1883)
