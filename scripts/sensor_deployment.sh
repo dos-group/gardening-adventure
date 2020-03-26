0207ba06c47daf96ef9181e6da01956ff701a9c7
peewee <peewee.manzer@outlook.de>
2020-03-26 21:03:54 +0100

    Add default IP/Node

--- scripts/sensor_deployment.sh
+++ scripts/sensor_deployment.sh
@@ -17,101 +17,88 @@ all_sensor () {
   dht_sensor
   moisture_sensor
   waterlevel_sensor
-  camera_sensor
 }
 
 ldr_sensor () {
-  1>&2 echo "Please enter label for the nodeselection"
-  read nodeName
-  node_exists
 
   1>&2 echo "Please enter name for the pod"
   read podName
 
-  1>&2 echo "Please enter the mqtt broker IP"
-  read brokerIP
+
   source /dev/stdin <<<"$(echo 'cat <<EOF >./ldr_sensor/final.yml'; cat ./ldr_sensor/ldr.yml; echo EOF;)"
   kubectl apply -f  ./ldr_sensor/final.yml
   rm ./ldr_sensor/final.yml
+  case_func
 }
 
 rgb_light () {
-  1>&2 echo "Please enter node name for the nodeselection"
-  read nodeName
-  node_exists
-
   1>&2 echo "Please enter name for the pod"
   read podName
 
-  1>&2 echo "Please enter the mqtt broker IP"
-  read brokerIP
   source /dev/stdin <<<"$(echo 'cat <<EOF >./light/final.yml'; cat ./light/rgb_light.yml; echo EOF;)"
   kubectl apply -f  ./light/final.yml
   rm ./light/final.yml
+  case_func
 }
 
 dht_sensor () {
-  1>&2 echo "Please enter node name for the nodeselection"
-  read nodeName
-  node_exists
-
   1>&2 echo "Please enter name for the pod"
   read podName
 
-  1>&2 echo "Please enter the mqtt broker IP"
-  read brokerIP
   source /dev/stdin <<<"$(echo 'cat <<EOF >./dht_sensor/final.yml'; cat ./dht_sensor/dht.yml; echo EOF;)"
   kubectl apply -f  ./dht_sensor/final.yml
   rm ./dht_sensor/final.yml
+  case_func
 }
 
 moisture_sensor () {
-  1>&2 echo "Please enter node name for the nodeselection"
-  read nodeName
-  node_exists
-
   1>&2 echo "Please enter name for the pod"
   read podName
 
-  1>&2 echo "Please enter the mqtt broker IP"
-  read brokerIP
   source /dev/stdin <<<"$(echo 'cat <<EOF >./moisture_sensor/final.yml'; cat ./moisture_sensor/moisture.yml; echo EOF;)"
   kubectl apply -f  ./moisture_sensor/final.yml
   rm ./moisture_sensor/final.yml
+  case_func
 }
 waterlevel_sensor () {
-  1>&2 echo "Please enter node name for the nodeselection"
-  read nodeName
-  node_exists
-
   1>&2 echo "Please enter name for the pod"
   read podName
 
-  1>&2 echo "Please enter the mqtt broker IP"
-  read brokerIP
   source /dev/stdin <<<"$(echo 'cat <<EOF >./waterlevel_sensor/final.yml'; cat ./waterlevel_sensor/waterlevel.yml; echo EOF;)"
   kubectl apply -f  ./waterlevel_sensor/final.yml
   rm ./waterlevel_sensor/final.yml
+  case_func
 }
 
 camera_sensor () {
-  1>&2 echo "Please enter node name for the nodeselection"
-  read nodeName
-  node_exists
-
   1>&2 echo "Please enter name for the pod"
   read podName
 
   source /dev/stdin <<<"$(echo 'cat <<EOF >./camera_module/final.yml'; cat ./camera_module/camera_module.yml; echo EOF;)"
   kubectl apply -f  ./camera_module/final.yml
   rm ./camera_module/final.yml
+  case_func
+}
+
+change_node (){
+  1>&2 echo "Please enter label for the nodeselection"
+  read nodeName
+  node_exists
+  1>&2 echo "New nodeselection ---> $nodeName"
+  case_func
+}
+
+change_IP (){
+  1>&2 echo "Please enter the mqtt broker IP"
+  read brokerIP
+  1>&2 echo "New broker IP ---> $brokerIP"
+  case_func
 }
 
 
 
-
-
 case_func () {
+  1>&2 echo "-----OPTIONS-----"
   1>&2 echo "Deploy all devices: 0"
   1>&2 echo "Deploy ldr sensor: 1"
   1>&2 echo "Deploy light device: 2"
@@ -119,10 +106,11 @@ case_func () {
   1>&2 echo "Deploy moisture sensor: 4"
   1>&2 echo "Deploy waterlevel sensor: 5"
   1>&2 echo "Deploy camera sensor: 6"
-
+  1>&2 echo "Change deploy node: 7"
+  1>&2 echo "Change Broker IP: 8"
   read option
   case "$option" in
-    0) 1>&2 echo "-0 deploy all devices " 
+    0) 1>&2 echo "-0 deploy all devices without camera" 
        all_sensor;; 
     1) 1>&2 echo "-1 deploy ldr sensor " 
        ldr_sensor;; 
@@ -136,6 +124,10 @@ case_func () {
        waterlevel_sensor;; 
     6) 1>&2 echo "-6 deploy camera sensor " 
        camera_sensor;; 
+    7) 1>&2 echo "-7 Change deploy node " 
+       change_node;; 
+    8) 1>&2 echo "-8 Change Broker IP " 
+       change_IP;; 
     *) 1>&2 echo "Option $option not recognized"
        1>&2 echo "" 
        case_func;;
@@ -144,6 +136,26 @@ case_func () {
    
   	shift
   }
+if [ ! -z "$1" ]
+  then
+    brokerIP=$1
+    1>&2 echo "Default Broker IP: $brokerIP"
+  else
+    1>&2 echo "Please enter the mqtt broker IP"
+    read brokerIP 
+fi
+
+if [ ! -z "$2" ]
+  then
+    nodeName=$2
+    1>&2 echo "Default Nodename: $nodeName"
+    node_exists 
+  else
+    1>&2 echo "Please enter label for the nodeselection"
+    read nodeName
+    node_exists
+fi
+
 case_func
 
 
